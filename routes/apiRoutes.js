b8ae790ded4c949e9cae9c5648972cc3f473bc59
smkskr <saumik.sarkar30@gmail.com>
2022-03-10 11:36:35 +0100

    removed getstudentsbyrollnum and getstudentsbyclassName

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,19 +1,11 @@
 const express = require("express");
 const createTask = require("./api/createTask");
-const getStudentsByClassName = require("./api/getStudentsByClassName");
-const getStudentsByRollNum = require("./api/getStudentsByRollNum");
 const getTasks = require("./api/getTasks");
 const router = express.Router();
 
 
-//get all students
+//get all tasks
 router.get("/tasks",getTasks);
-
-//get student by roll Num
-router.get("/student/:rollNum" , getStudentsByRollNum)
-
-//get student by class name
-router.get("/student/class/:name" , getStudentsByClassName)
 
 //create task
 router.post("/addtask",createTask)
